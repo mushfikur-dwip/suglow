c83bb6c19d6f5dba6166d2e95f66003d240a7bac
mushfikur-dwip <musfikurrahmandip@gmail.com>
2026-01-05 04:32:29 +0600

    Remove backend folder, now in separate repo

--- backend/database/update_product_slugs.sql
+++
@@ -1,7 +0,0 @@
--- Update existing products to add slugs if they don't have them
-UPDATE products 
-SET slug = LOWER(REPLACE(REPLACE(REPLACE(REPLACE(name, ' ', '-'), '\'', ''), '%', 'percent'), '+', 'plus'))
-WHERE slug IS NULL OR slug = '';
-
--- Verify all products have slugs
-SELECT id, name, slug FROM products;
